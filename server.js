2dcc7d288d0916a15f8625300eee15573fcc9033
nghochi123 <nghochi123@gmail.com>
2022-02-26 15:23:30 +0800

    push more

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-var app = require("express")();
-var http = require("http").createServer(app);
+const app = require("express")();
+const http = require("http").createServer(app);
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
+const { shuffle } = require("./utils/misc");
+
+const wordlist1 = require("./words/common.json").commonWords;
+const wordlist2 = require("./words/common2.json").commonWords;
+shuffle(wordlist1);
+shuffle(wordlist2);
+
+// SOURCE: https://github.com/dariusk/corpora/blob/master/data/words/common.json
 
 // CORS FUCK
-var io = require("socket.io")(http, {
+const io = require("socket.io")(http, {
   cors: { origin: "*", methods: ["GET", "POST"] },
 });
 
@@ -13,6 +27,43 @@ http.listen(PORT, () => {
 });
 
 io.on("connection", (socket) => {
+  // Everything goes into here
   console.log("New Client Connected");
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
+    if (error) {
+      return callback(error);
+    }
+    socket.join(user.room);
+    // socket.broadcast.to(room).emit() < Let the other user know that someone has joined.
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+      p1Wl: wordlist1,
+      p2W2: wordlist2,
+    });
+    callback();
+  });
+  socket.on("typing", ({ text }, callback) => {
+    const user = getUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("typeResponse", {
+        ptext: text,
+        player: user.player,
+      });
+    }
+  });
+  socket.on("completedWord", () => {});
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    if (user) {
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
+  });
   socket.emit("connection", null);
 });
+
+// TODO: GET POINT SYSTEM, WORDLIST,
---
+++ utils/users.js
@@ -0,0 +1,68 @@
+const users = [];
+
+//addUser
+const addUser = ({ id, username, room }) => {
+  username = username.trim().toLowerCase();
+  room = room.trim().toLowerCase();
+  //Validate data
+  if (!username || !room) {
+    return {
+      error: "Username and room are required!",
+    };
+  }
+  //Check for existing user
+  const existingUser = users.find((user) => {
+    return user.room === room && user.username === username;
+  });
+  if (existingUser) {
+    return {
+      error: "Username is in use!",
+    };
+  }
+  // If 2 people are already in a room:
+  let player = 1;
+  if (users.length == 2) {
+    return {
+      error: "There are already two people in this room! Create another room.",
+    };
+  } else if (users.length == 1) {
+    player = 2;
+  }
+  //Store user
+  const user = { id, username, room, player };
+  users.push(user);
+  return {
+    user,
+  };
+};
+
+//removeUser
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+//getUser
+const getUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users[index];
+  }
+  return undefined;
+};
+
+//getUsersInRoom
+const getUsersInRoom = (room) => {
+  room = room.trim().toLowerCase();
+  return (usersInRoom = users.filter((user) => user.room === room));
+};
+
+module.exports = {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+};
